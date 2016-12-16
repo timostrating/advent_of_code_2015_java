147b526600b0d049dd97b2f2847ed78b0c6f67b1
Timo Strating <Timostrating@home.nl>
2016-12-16 22:20:05 +0100

    day 10 part2

---
+++ src/day10/part2.java
@@ -0,0 +1,10 @@
+package day10;
+
+/**
+ * Created by Sneeuwpopsneeuw on 08-Dec-16.
+ */
+public class part2 {
+    public static void main(String[] args) {
+        new part1().start("1113122113", 50);
+    }
+}
